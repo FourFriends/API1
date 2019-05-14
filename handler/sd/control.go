c0f61b8cb9663f4eae462df5838bcd3196ac1d58
Your Name <you@example.com>
2019-05-14 10:55:21 +0800

    增加错误日志文件信息。增加接口

--- handler/sd/control.go
+++ handler/sd/control.go
@@ -1,14 +1,14 @@
 package sd
 
 import (
-"fmt"
-"net/http"
+	"fmt"
+	"net/http"
 
-"github.com/gin-gonic/gin"
-"github.com/shirou/gopsutil/cpu"
-"github.com/shirou/gopsutil/disk"
-"github.com/shirou/gopsutil/load"
-"github.com/shirou/gopsutil/mem"
+	"github.com/gin-gonic/gin"
+	"github.com/shirou/gopsutil/cpu"
+	"github.com/shirou/gopsutil/disk"
+	"github.com/shirou/gopsutil/load"
+	"github.com/shirou/gopsutil/mem"
 )
 
 const (
--- router/view.go
+++ router/view.go
@@ -1,19 +1,24 @@
 package router
 
 import (
-	"API1/sd"
+	"API1/handler/sd"
+	"API1/handler/user"
 	"github.com/gin-gonic/gin"
 )
 
-
-
-func Load(g *gin.Engine) *gin.Engine{
+func Load(g *gin.Engine) *gin.Engine {
 	svcd := g.Group("/v1")
 	{
-		svcd.GET("/health",sd.HealthCheck)
-		svcd.GET("/disk",sd.DiskCheck)
-		svcd.GET("/cpu",sd.CPUCheck)
-		svcd.GET("/ram",sd.RAMCheck)
+		svcd.GET("/health", sd.HealthCheck)
+		svcd.GET("/disk", sd.DiskCheck)
+		svcd.GET("/cpu", sd.CPUCheck)
+		svcd.GET("/ram", sd.RAMCheck)
 	}
-	return  g
+
+	u := g.Group("/v1/user")
+	{
+		u.POST("", user.Create)
+	}
+
+	return g
 }
